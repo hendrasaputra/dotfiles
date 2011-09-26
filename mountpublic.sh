0581da49775d562e00f6fc31b0fa2f5fbe38fbd1
Hendra Saputra <hendrasaputra@gmail.com>
2011-09-26 21:32:38 +0700

    Add new .bash_profile and fixed mountpublic.sh

--- mountpublic.sh
+++ mountpublic.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+if [ ! -e /Volumes/public ];then mkdir /Volumes/public; fi
 sudo mount -t nfs -o resvport,soft,intr,rsize=8192,wsize=8192,timeo=900,retrans=3,proto=tcp 192.168.3.100:/mnt/data/public /Volumes/public
