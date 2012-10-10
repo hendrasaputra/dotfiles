f038d4137316c827cb04d2aa5567856dd63bf4d1
Hendra Saputra <hendrasaputra@gmail.com>
2012-10-10 15:31:44 +0700

    Change bootstrap.sh to zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ read -p "This may overwrite existing files in your home directory. Are you sure?
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+  cp vimrc ~/.vimrc
 fi
-source "$HOME/.bash_profile"
+source "$HOME/.zshrc"
